af532e34e0120d606b0f81fc6eee40e856af72a7
tarasvolianskyi <volianskyitv@gmail.com>
2018-09-11 13:54:01 +0300

    update style

--- app/src/main/java/com/example/t/cpm_calc/MainActivity.java
+++ app/src/main/java/com/example/t/cpm_calc/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvSpin;
@@ -28,12 +30,18 @@ public class MainActivity extends AppCompatActivity {
     private Spinner spinnerCateg4;
     private Spinner spinnerCateg5;
 
+    private int num1;
+    private int num2;
+    private int num3;
+    private int num4;
+    private int num5;
+
     private String[] typeOfTraffic = {"betting", "sport", "adlt", "mainstream"};
     private String[] typeOfContact = {"Skype", "e-mail", "tel", "Telegram", "Messenger FB"};
     private String[] typeOfPositions = {"кэтфиш", "баннер", "брендирование", "кликандер", "фуллскрин",
             "видео", "кэфы", "обзор", "статья"
             , "рейтинг", "озвучка", " спонсорство сайта", "ютуб", "соц сети", "поп-ап"};
-    private String[] typeOfCategor = {"sport-betting", "adult", "mainstream"};
+    private String[] typeOfCategor = {"sport-betting", "adlt", "mainstream"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +63,63 @@ public class MainActivity extends AppCompatActivity {
         initCPM3();
         initCPM4();
         initCPM5();
+        String st1 = "баннер";
+        String st2 = "mainstream";
+        int numOfMet = choseMethod(st1, st2);
+    }
+
+    private int choseMethod(String st1, String st2) {
+        double koefMin1 = 0.8;
+        double koefMin2 = 0.5;
+        double koefMin3 = 1.2;
+        double koefMin4 = 0.2;
+        double koefMin5 = 0.3;
+        double koefMin6 = 0.5;
+        double koefMin7 = 0.2;
+        double koefMin8 = 0.3;
+        double koefMin9 = 0.5;
+        double koefMin10 = 0.2;
+        double koefMin11 = 0.3;
+        double koefMin12 = 0.5;
+        double koefMin13 = 0.7;
+        double koefMin14 = 0.4;
+        double koefMin15 = 0.7;
+        double koefMin16 = 0.9;
+        double koefMin17 = 0.4;
+        double koefMin18 = 0.7;
+        double koefMin19 = 0.9;
+        double koefMin20 = 0.4;
+        double koefMin21 = 0.7;
+        double koefMin22 = 0.9;
+
+        double koefMax1 = 1.2;
+        double koefMax2 = 1;
+        double koefMax3 = 1.5;
+        double koefMax4 = 0.5;
+        double koefMax5 = 0.7;
+        double koefMax6 = 0.9;
+        double koefMax7 = 0.5;
+        double koefMax8 = 0.7;
+        double koefMax9 = 0.9;
+        double koefMax10 = 0.5;
+        double koefMax11 = 0.6;
+        double koefMax12 = 0.9;
+        double koefMax13 = 1.5;
+        double koefMax14 = 0.8;
+        double koefMax15 = 1;
+        double koefMax16 = 1.2;
+        double koefMax17 = 0.8;
+        double koefMax18 = 1;
+        double koefMax19 = 1.2;
+        double koefMax20 = 0.8;
+        double koefMax21 = 1;
+        double koefMax22 = 1.2;
+
+
+        if (st1 == "Кликандер" && st2 == "adult") {
+            num1 = 1;
+        }
+        return 0;
     }
 
     private void initCPM5() {
